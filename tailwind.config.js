99030f15eb2e44ff82815a767bd5ceba4a291d33
Daniel <danieldimitrov2304@gmail.com>
2024-11-19 21:49:05 +0200

    Init 🚀

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+// Tailwind CSS configuration file
+export default {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}", // Ensures Tailwind scans all your files
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
