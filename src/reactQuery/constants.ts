ee6ab5e782b74c9c7436389581aaf4391ceb13f1
Ivaylo V Ivanov <88397502+Daooobg@users.noreply.github.com>
2024-11-28 07:58:38 +0000

    Add placeholders for urlKeys and queryKeys

---
+++ src/reactQuery/constants.ts
@@ -0,0 +1,11 @@
+// Note: This file is used to store all the constants used in the react-query module
+
+// urlKeys: This object is used to store all the keys used in the url object in the useQuery hook
+export const urlKeys = {
+
+};
+
+// queryKeys: This object is used to store all the keys used in the queryKey object in the useQuery hook
+export const queryKeys = {
+
+};
