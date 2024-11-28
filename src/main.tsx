e479cd9e82722a967ea8588ec72d0f92163a21f4
Ivaylo V Ivanov <88397502+Daooobg@users.noreply.github.com>
2024-11-28 08:07:47 +0000

    Add QueryClientProvider and ReactQueryDevtools

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,18 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import { QueryClientProvider } from '@tanstack/react-query';
+import { queryClient } from './reactQuery/queryClient.ts';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
+
+import App from './App.tsx';
+
+import './index.css';
 
 createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <StrictMode>
+        <QueryClientProvider client={queryClient}>
+            <App />
+            <ReactQueryDevtools />
+        </QueryClientProvider>
+    </StrictMode>
+);
