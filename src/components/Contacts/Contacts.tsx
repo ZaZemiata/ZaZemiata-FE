e72f304ac7376c0989bffc61c346b8640b5597c4
Silviya Georgieva <117118888+silviqkamenova1@users.noreply.github.com>
2024-12-05 11:00:24 +0200

    created navigation bar

---
+++ src/components/Contacts/Contacts.tsx
@@ -0,0 +1,9 @@
+const Contacts = () => {
+    return (
+        <div className="flex items-center justify-center h-[75vh]">
+            <h1>Контакти</h1>
+        </div>
+    );
+}
+
+export default Contacts;
---
+++ src/components/WhatWeDo/WhatWeDo.tsx
@@ -0,0 +1,9 @@
+const WhatWeDo = () => {
+    return (
+        <div className="flex items-center justify-center h-[75vh]">
+            <h1>Какво правим</h1>
+        </div>
+    );
+}
+
+export default WhatWeDo;
