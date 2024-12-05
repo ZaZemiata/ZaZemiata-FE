173836cf5bcc2b8aab7a43a11f2033a7946ed0f0
Silviya Georgieva <117118888+silviqkamenova1@users.noreply.github.com>
2024-12-05 11:07:00 +0200

    created navigation bar

--- src/router.tsx
+++ src/router.tsx
@@ -1,17 +1,29 @@
-import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import Home from './pages/Home';
-import About from './pages/About';
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Home from "./components/Home/Home";
+import About from "./components/About/About";
+import WhatWeDo from "./components/WhatWeDo/WhatWeDo";
+import News from "./components/News/News";
+import Resources from "./components/Resources/Resources";
+import GetInvolved from "./components/GetInvolved/GetInvolved";
+import Contacts from "./components/Contacts/Contacts";
 
-const AppRouter: React.FC = () => {
-    return (
-        <Router>
-            <Routes>
-                <Route path="/" element={<Home />} />
-                <Route path="/about" element={<About />} />
-            </Routes>
-        </Router>
-    );
-};
+function App() {
+    const router = createBrowserRouter([
+        {
+            path: "/",
+            element: <Home />, // Shared layout with the navigation bar
+            children: [
+                { path: "about", element: <About /> }, // За нас
+                { path: "what-we-do", element: <WhatWeDo /> }, // Какво правим
+                { path: "news", element: <News /> }, // Новини
+                { path: "resources", element: <Resources /> }, // Ресурси
+                { path: "get-involved", element: <GetInvolved /> }, // Включи се
+                { path: "contact", element: <Contacts /> }, // Контакти
+            ],
+        },
+    ]);
 
-export default AppRouter;
+    return <RouterProvider router={router} />;
+}
+
+export default App;
