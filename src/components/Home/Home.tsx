4c79efbfa92c3969ff395f991df993f1928ef807
Ivaylo V Ivanov <88397502+Daooobg@users.noreply.github.com>
2024-12-09 10:17:30 +0000

    Refactor home component

--- src/components/Home/Home.tsx
+++ src/components/Home/Home.tsx
@@ -1,20 +1,7 @@
-import { Outlet } from "react-router-dom";
-import NavigationBar from "../NavigationBar/NavigationBar";
-
 const Home: React.FC = () => {
     return (
-        <div className="flex flex-col min-h-screen">
-          
-            <NavigationBar />
-            
-            <main className="flex-grow p-4 px-20">
-                <Outlet /> 
-            </main>
-
-
-            <footer className="text-center py-4 bg-[#19AD52]">
-                <p>© 2024 За Земята</p>
-            </footer>
+        <div className="flex items-center justify-center h-[75vh]">
+            <h1>Home Page</h1>
         </div>
     );
 };
