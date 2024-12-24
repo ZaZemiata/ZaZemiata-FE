c89bd82aa4a6cb116d2fd14e706f935aa2ef0ed2
Ivaylo V Ivanov <88397502+Daooobg@users.noreply.github.com>
2024-12-24 09:48:07 +0000

    Add envDir and alias

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,17 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import path from "path";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  css: {
-    postcss: './postcss.config.js'
-  }
-})
+    plugins: [react()],
+    envDir: "./environment",
+    resolve: {
+        alias: {
+            "@": path.join(__dirname, "src/"),
+        },
+    },
+    css: {
+        postcss: "./postcss.config.js",
+    },
+});
