0e3bef38f0286cb179c2be553a364a4ca93c8066
Ivaylo V Ivanov <88397502+Daooobg@users.noreply.github.com>
2024-12-24 10:57:13 +0000

    Add crawled data constants

--- src/reactQuery/constants.ts
+++ src/reactQuery/constants.ts
@@ -2,10 +2,12 @@
 
 // urlKeys: This object is used to store all the keys used in the url object in the useQuery hook
 export const urlKeys = {
-
+    get: {
+        crawledData: "/api/crawled-data/filter",
+    },
 };
 
 // queryKeys: This object is used to store all the keys used in the queryKey object in the useQuery hook
 export const queryKeys = {
-
+    crawledData: "Crawled Data",
 };
