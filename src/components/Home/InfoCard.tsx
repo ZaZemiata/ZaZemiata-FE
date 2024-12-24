1881ed944b53bbe6a1186271767ae2a350117793
Ivaylo V Ivanov <88397502+Daooobg@users.noreply.github.com>
2024-12-24 11:03:19 +0000

    Refactor

--- src/components/Home/InfoCard.tsx
+++ src/components/Home/InfoCard.tsx
@@ -1,17 +1,21 @@
+import dayjs from "dayjs";
+import "dayjs/locale/bg";
+
 type InfoCardProps = {
-    title: string;
+    // title: string;
     contractor: string;
-    date: string;
+    date: Date;
     text: string;
 };
 
-const InfoCard = ({ title, contractor, date, text }: InfoCardProps) => {
+const InfoCard = ({ contractor, date, text }: InfoCardProps) => {
+    const formattedDate = dayjs(date).locale("bg").format("D MMMM YYYY");
     return (
-        <div className="max-w-[75rem] rounded-[36px] border-2 border-[#19ad52]">
+        <div className="w-full rounded-[36px] border-2 border-[#19ad52]">
             <div className="flex gap-4 items-center py-4 my-4 mx-9 border-b border-[#19ad52] text-[#3b3b3b]">
-                <h2 className="text-xl font-bold leading-tight">{title}</h2>
+                <h2 className="text-xl font-bold leading-tight">Title</h2>
                 <p className="text-base font-normal leading-none">{contractor}</p>
-                <p className="text-base font-normal leading-none">{date}</p>
+                <p className="text-base font-normal leading-none">{formattedDate}</p>
             </div>
             <p className="my-4 mx-9 text-base font-normal leading-normal">{text}</p>
         </div>
