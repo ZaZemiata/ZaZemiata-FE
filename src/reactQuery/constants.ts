d633e812f9d5404e1d4d4fed01047013c9bd8290
Ivaylo V Ivanov <88397502+Daooobg@users.noreply.github.com>
2024-12-27 10:36:46 +0000

    Add new url key for login

--- src/reactQuery/constants.ts
+++ src/reactQuery/constants.ts
@@ -5,6 +5,9 @@ export const urlKeys = {
     get: {
         crawledData: "/api/crawled-data/filter",
     },
+    post: {
+        login: "/login",
+    },
 };
 
 // queryKeys: This object is used to store all the keys used in the queryKey object in the useQuery hook
