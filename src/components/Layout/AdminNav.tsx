7f5226c3b915e11e30bdd49d5e72a8831cd2de6b
Ivaylo V Ivanov <88397502+Daooobg@users.noreply.github.com>
2024-12-28 12:59:54 +0000

    Refactor AdminNav component with dynamic tab rendering and active state management

--- src/components/Layout/AdminNav.tsx
+++ src/components/Layout/AdminNav.tsx
@@ -1,14 +1,41 @@
-import React, { useState } from "react";
-import { Link, Outlet } from "react-router-dom";
+import { UserDataType } from "@/share/types";
+import cn from "@/util/cn";
+
+import React, { useEffect, useState } from "react";
+import { Link, Outlet, useLoaderData, useLocation } from "react-router-dom";
 
 const AdminNav: React.FC = () => {
-    const [activeTab, setActiveTab] = useState("Register User");
+    // get the location hook from react-router-dom
+    const location = useLocation();
 
-    const tabs = [
-        { name: "Register User", path: "/dashboard/register" },
-        { name: "Keywords", path: "/dashboard/keywords" },
-        { name: "Sync Settings", path: "/dashboard/settings" },
+    // function to get the current path and return the second path
+    const getLocationPath = (path: string) => {
+        const pathArray = path.split("/").filter((x) => x);
+        return pathArray[1];
+    };
+
+    // set the active tab based on the current path
+    const [activeTab, setActiveTab] = useState("");
+
+    // get the user data from the loader data
+    const userData = useLoaderData() as UserDataType;
+
+    // tabs base user
+    const baseTabs = [
+        { name: "Keywords", path: "/dashboard/keywords", tab: "keywords" },
+        { name: "Sync Settings", path: "/dashboard/settings", tab: "settings" },
     ];
+
+    // tabs for admin
+    const adminTabs = [{ name: "Register User", path: "/dashboard/register", tab: "register" }, ...baseTabs];
+
+    // set the tabs based on the user data
+    const tabs = userData.is_admin ? adminTabs : baseTabs;
+
+    // set the active tab based on the current path
+    useEffect(() => {
+        setActiveTab(getLocationPath(location.pathname));
+    }, [location.pathname]);
 
     return (
         <div className="container mx-auto px-4 py-4">
@@ -17,10 +44,10 @@ const AdminNav: React.FC = () => {
                     <Link
                         key={tab.name}
                         to={tab.path}
-                        className={`px-4 py-2 cursor-pointer text-lg ${
-                            activeTab === tab.name ? "text-black border-b-2 border-black font-bold" : "text-gray-400"
-                        }`}
-                        onClick={() => setActiveTab(tab.name)}
+                        className={cn(
+                            activeTab === tab.tab ? "text-black border-b-2 border-black font-bold" : "text-gray-400",
+                            "px-4 py-2 cursor-pointer text-lg"
+                        )}
                     >
                         {tab.name}
                     </Link>
