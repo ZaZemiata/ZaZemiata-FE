16b3c40940628e61578ddfa0b4e32e7a745f3eee
Ivaylo V Ivanov <88397502+Daooobg@users.noreply.github.com>
2024-12-29 16:53:09 +0000

    Add Keywords component with search, add, and keyword list functionality

--- src/components/Dashboard/Keywords/Keywords.tsx
+++ src/components/Dashboard/Keywords/Keywords.tsx
@@ -1,5 +1,37 @@
+import AddKeyWords from "./AddKeyWords";
+import useGetKeywords from "./hooks/useGetKeywords";
+import KeyWordCard from "./KeyWordCard";
+
 const Keywords = () => {
-    return <p>Keywords</p>;
+    const { handleKeyWord, keywordFilter, keywords } = useGetKeywords();
+
+    return (
+        <div>
+            <ul className="p-4 border-b border-gray-200 flex items-center gap-7">
+                <li>
+                    <input
+                        className="max-w-52 outline-none border-b border-gray-300 p-2 ml-10"
+                        type="text"
+                        value={keywordFilter}
+                        onChange={(e) => handleKeyWord(e.target.value)}
+                        placeholder="Search"
+                    />
+                </li>
+                <li>
+                    <AddKeyWords />
+                </li>
+            </ul>
+            <div className="grid grid-cols-[5rem_1fr_10rem_5rem_1fr] items-center p-4 border-b border-gray-200">
+                <input type="checkbox" />
+                <p>Ключова дума</p>
+                <p>Тежест</p>
+                <p>Активност</p>
+                <p>Действия</p>
+            </div>
+            {keywords &&
+                keywords.map((keyword) => <KeyWordCard key={keyword.id} {...keyword} keyWordFilter={keywordFilter} />)}
+        </div>
+    );
 };
 
 export default Keywords;
