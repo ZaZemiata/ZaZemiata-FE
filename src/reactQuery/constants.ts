63cef3ebda48edabfd40adcfe77aa1c805c3311e
Ivaylo V Ivanov <88397502+Daooobg@users.noreply.github.com>
2024-12-29 16:48:02 +0000

    Add urlKeys and queryKeys

--- src/reactQuery/constants.ts
+++ src/reactQuery/constants.ts
@@ -4,13 +4,19 @@
 export const urlKeys = {
     get: {
         crawledData: "/api/crawled-data/filter",
+        keywords: "/api/keywords",
     },
     post: {
         login: "/login",
+        addKeyWord: "/api/keyword/add"
     },
+    patch: {
+        updateKeyword: "/api/keyword/update"
+    }
 };
 
 // queryKeys: This object is used to store all the keys used in the queryKey object in the useQuery hook
 export const queryKeys = {
     crawledData: "Crawled Data",
+    keywords: "Keywords",
 };
