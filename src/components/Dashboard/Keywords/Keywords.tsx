f12f69f00aa40a9b2255a39b5dd340b189a6dd22
Ivaylo V Ivanov <88397502+Daooobg@users.noreply.github.com>
2025-01-03 08:51:10 +0000

    Add Cyrillic and digits validation for keyword search input

--- src/components/Dashboard/Keywords/Keywords.tsx
+++ src/components/Dashboard/Keywords/Keywords.tsx
@@ -1,14 +1,26 @@
 import { useState } from "react";
 import AddKeyWords from "./AddKeyWords";
 import useGetKeywords from "./hooks/useGetKeywords";
-import KeyWordCard from "./KeyWordCard";
+import KeyWordCard from "./KeywordCard";
 
 const Keywords = () => {
     // useGetKeywords hook to get the keywords
     const { handleKeyWord, keywordFilter, keywords } = useGetKeywords();
+    const [searchError, setSearchError] = useState(""); // state to check if the search input is valid
 
     // state to check all the checkboxes
     const [isCheckAll, setIsCheckAll] = useState(false);
+
+    const validateCyrillicAndDigitsInput = (value: string) => {
+        const cyrillicAndDigitsRegex = /^[\u0400-\u04FF0-9\s]*$/; // Regex to match Cyrillic characters, digits, and spaces
+        if (!cyrillicAndDigitsRegex.test(value)) {
+            setSearchError("Моля използвайте само символи на кирилица и цифри."); // Display error message
+            return;
+        } else {
+            setSearchError(""); // Clear error message
+        }
+        handleKeyWord(value); // Pass valid input to handleKeyWord function
+    };
 
     return (
         <div>
@@ -18,13 +30,14 @@ const Keywords = () => {
                         className="max-w-52 outline-none border-b border-gray-300 p-2 ml-10"
                         type="text"
                         value={keywordFilter}
-                        onChange={(e) => handleKeyWord(e.target.value)}
+                        onChange={(e) => validateCyrillicAndDigitsInput(e.target.value)}
                         placeholder="Search"
                     />
                 </li>
                 <li>
                     <AddKeyWords />
                 </li>
+                {searchError && <p className="text-red-500 text-nowrap">{searchError}</p>}
             </ul>
             <div className="grid grid-cols-[5rem_1fr_10rem_5rem_1fr] items-center p-4 border-b border-gray-200">
                 <input type="checkbox" checked={isCheckAll} onClick={() => setIsCheckAll((prev) => !prev)} />
@@ -33,8 +46,13 @@ const Keywords = () => {
                 <p className="px-5">Активност</p>
                 <p className="px-5 text-end">Действия</p>
             </div>
-            {keywords &&
-                keywords.map((keyword) => <KeyWordCard key={keyword.id} {...keyword} keyWordFilter={keywordFilter} checked={isCheckAll} />)}
+            {keywords && keywords.length > 0 ? (
+                keywords.map((keyword) => (
+                    <KeyWordCard key={keyword.id} {...keyword} keyWordFilter={keywordFilter} checked={isCheckAll} />
+                ))
+            ) : (
+                <p className="text-center text-xl mt-16 ">Не са намерени ключови думи спрямо зададения критерии</p>
+            )}
         </div>
     );
 };
