b6db66eb424acab4e2f83d77a1cda31382400d54
Ivaylo V Ivanov <88397502+Daooobg@users.noreply.github.com>
2025-01-15 08:35:39 +0000

    Refactor

--- src/components/Home/InfoCard.tsx
+++ src/components/Home/InfoCard.tsx
@@ -2,20 +2,26 @@ import dayjs from "dayjs";
 import "dayjs/locale/bg";
 
 type InfoCardProps = {
-    // title: string;
     contractor: string;
     date: Date;
     text: string;
+    SourceUrls: {
+        Sources: {
+            display_name: string;
+        };
+    };
 };
 
-const InfoCard = ({ contractor, date, text }: InfoCardProps) => {
+const InfoCard = ({ contractor, date, text, SourceUrls }: InfoCardProps) => {
     const formattedDate = dayjs(date).locale("bg").format("D MMMM YYYY");
     return (
         <div className="w-full rounded-[36px] border-2 border-[#19ad52]">
-            <div className="flex gap-4 items-center py-4 my-4 mx-9 border-b border-[#19ad52] text-[#3b3b3b]">
-                <h2 className="text-xl font-bold leading-tight">Title</h2>
-                <p className="text-base font-normal leading-none">{contractor}</p>
-                <p className="text-base font-normal leading-none">{formattedDate}</p>
+            <div className="flex gap-4 items-center py-4 my-4 mx-9 border-b border-[#19ad52] text-[#3b3b3b] justify-between">
+                <div className="flex gap-4">
+                    <p className="text-base font-normal leading-none">{SourceUrls.Sources.display_name}</p>
+                    <p className="text-base font-normal leading-none">{formattedDate}</p>
+                </div>
+                <p className="text-base font-bold leading-none">{contractor}</p>
             </div>
             <p className="my-4 mx-9 text-base font-normal leading-normal">{text}</p>
         </div>
